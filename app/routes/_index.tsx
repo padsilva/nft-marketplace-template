9653ed8fd62d591d0799b8b740d901777e17072d
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2023-10-10 16:55:54 +0100

    trending collection section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -76,12 +76,131 @@ export default function Index() {
               <div className="flex gap-3">
                 <img
                   src="/assets/men_avatar.png"
-                  alt="space hero"
+                  alt="men avatar"
                   className="rounded-full"
                   width={24}
                   height={24}
                 />
                 <p>Animakid</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* TRENDING COLLECTION SECTION */}
+        <div className="flex flex-col py-20 px-32 gap-[60px]">
+          <div className="self-start">
+            <h3 className="text-[38px] font-semibold">Trending Collection</h3>
+            <p className="text-[22px] capitalize">
+              Checkout our weekly updated trending collection.
+            </p>
+          </div>
+          <div className="grid grid-cols-3 items-center justify-items-center gap-[30px]">
+            <div className="flex flex-col gap-[15px] items-center">
+              <img
+                src="/assets/dog.png"
+                alt="dog"
+                className="rounded-[20px] max-h-[330px]"
+              />
+              <div className="grid grid-cols-3 gap-[15px]">
+                <img
+                  src="/assets/cat.png"
+                  alt="cat"
+                  className="rounded-[20px]"
+                />
+                <img
+                  src="/assets/bear.png"
+                  alt="bear"
+                  className="rounded-[20px]"
+                />
+                <div className="h-full w-full flex items-center justify-center bg-[#A259FF] rounded-[20px]">
+                  <h5 className="text-[22px] font-semibold">1025+</h5>
+                </div>
+              </div>
+              <div className="self-start flex flex-col gap-2.5">
+                <h5 className="text-[22px] font-semibold">Happy Robots</h5>
+                <div className="flex gap-3">
+                  <img
+                    src="/assets/men_avatar.png"
+                    alt="men avatar"
+                    className="rounded-full"
+                    width={24}
+                    height={24}
+                  />
+                  <p>Animakid</p>
+                </div>
+              </div>
+            </div>
+            {/* TODO: replace this duplicated information */}
+            <div className="flex flex-col gap-[15px] items-center">
+              <img
+                src="/assets/dog.png"
+                alt="dog"
+                className="rounded-[20px] max-h-[330px]"
+              />
+              <div className="grid grid-cols-3 gap-[15px]">
+                <img
+                  src="/assets/cat.png"
+                  alt="cat"
+                  className="rounded-[20px]"
+                />
+                <img
+                  src="/assets/bear.png"
+                  alt="bear"
+                  className="rounded-[20px]"
+                />
+                <div className="h-full w-full flex items-center justify-center bg-[#A259FF] rounded-[20px]">
+                  <h5 className="text-[22px] font-semibold">1025+</h5>
+                </div>
+              </div>
+              <div className="self-start flex flex-col gap-2.5">
+                <h5 className="text-[22px] font-semibold">Happy Robots</h5>
+                <div className="flex gap-3">
+                  <img
+                    src="/assets/men_avatar.png"
+                    alt="men avatar"
+                    className="rounded-full"
+                    width={24}
+                    height={24}
+                  />
+                  <p>Animakid</p>
+                </div>
+              </div>
+            </div>
+            {/* TODO: replace this duplicated information */}
+            <div className="flex flex-col gap-[15px] items-center">
+              <img
+                src="/assets/dog.png"
+                alt="dog"
+                className="rounded-[20px] max-h-[330px]"
+              />
+              <div className="grid grid-cols-3 gap-[15px]">
+                <img
+                  src="/assets/cat.png"
+                  alt="cat"
+                  className="rounded-[20px]"
+                />
+                <img
+                  src="/assets/bear.png"
+                  alt="bear"
+                  className="rounded-[20px]"
+                />
+                <div className="h-full w-full flex items-center justify-center bg-[#A259FF] rounded-[20px]">
+                  <h5 className="text-[22px] font-semibold">1025+</h5>
+                </div>
+              </div>
+              <div className="self-start flex flex-col gap-2.5">
+                <h5 className="text-[22px] font-semibold">Happy Robots</h5>
+                <div className="flex gap-3">
+                  <img
+                    src="/assets/men_avatar.png"
+                    alt="men avatar"
+                    className="rounded-full"
+                    width={24}
+                    height={24}
+                  />
+                  <p>Animakid</p>
+                </div>
               </div>
             </div>
           </div>
